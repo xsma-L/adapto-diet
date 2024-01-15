7f27a0230bb91ee8e984b48f14b10a417d1732dd
Ismael Traore <ismael.traore@epitech.eu>
2024-01-15 04:26:15 +0100

    Docker prisma-studio (setup-fix)

--- server/prisma/migrations/20240113043456_/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client/edge');
 
 const prisma = new PrismaClient();
 const app = express();
