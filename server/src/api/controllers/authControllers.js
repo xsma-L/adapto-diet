050f7acf3004ec9c72208f65d3610d0197afec2d
Ismael Traore <ismael.traore@epitech.eu>
2024-01-17 14:37:24 +0100

    Email verification (feature)

--- server/src/api/controllers/authControllers.js
+++ server/src/api/controllers/authControllers.js
@@ -2,13 +2,20 @@ const prisma = require("@prisma/client")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
+const mailer = require("../../nodemailer")
+
 const prismaClient = new prisma.PrismaClient()
 
 exports.signup = async (req, res) => {
-    const { first_name, last_name, email, password } = req.body
+    const { first_name, last_name, email, password } = req.body.data
     const hashedPassword = await bcrypt.hash(password, 12)
 
     try {
+        const verifyEmail = await prismaClient.user.findUnique({ where: { email } })
+        if(verifyEmail) {
+           return res.status(401).send('User already exist')
+        }
+
         const user = await prismaClient.user.create({
             data: {
                 first_name,
@@ -17,10 +24,45 @@ exports.signup = async (req, res) => {
                 password: hashedPassword
             }
         })
-        res.status(201).json({ message: 'User created', userId: user.id })
+        
+        if(user) {
+            const token = jwt.sign({ email: email }, process.env.JWT_SECRET, { expiresIn: '24h' })
+            const mailData = {
+                email: email,
+                name: first_name,
+                token: `http://localhost:3001/signup/confirmation/${token}`
+            }
+            await mailer.sendConfirmationMail(mailData)
+        }
+
+
+        res.status(201).json({ message: 'User created'})
     } catch (error) {
+        console.log(error)
         res.status(500).json({ message: 'Error creating user' })
     }
+}
+
+exports.emailConfirmation = async (req, res) => {
+    const { token } = req.body.data
+    const decoded = jwt.decode(token)
+
+    console.log('token data =>', token)
+
+    const updateUser = await prismaClient.user.update({
+        where: {
+            email: decoded.email
+        },
+        data: {
+            verified: true
+        }
+    })
+
+    if(!updateUser) {
+        res.status(500).json({ message: "Error verifing account" })
+    }
+
+    res.status(200).json({ message: "Account verified"})
 }
 
 exports.login = async (req, res) => {
