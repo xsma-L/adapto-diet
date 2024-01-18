128874b6b7ec7c23e844635713200660f9ca46d7
Ismael Traore <ismael.traore@epitech.eu>
2024-01-18 04:41:53 +0100

    Homepage image (fix)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -39,8 +39,8 @@ export default function Home() {
       </section>
       <section className="h-full w-full lg:h-fit lg:flex lg:justify-center lg:align-center">
         <div className="px-6 text-center mt-10 py-5 lg:mt-0 lg:w-[80%] lg:flex lg:flex-row-reverse lg:items-center lg:justify-around">
-          <div className="w-60 h-32 relative mt-3 mb-6 mx-auto lg:basis-2/5 lg:h-64 lg:mt-0 lg:mx-0">
-            <Image src="/Chars.webp" fill={true} alt="Panier équilibré repmlit de légumes et protéines" />
+          <div className="w-60 h-[11rem] relative mt-3 mb-6 mx-auto lg:basis-2/5 lg:h-[18rem] lg:mt-0 lg:mx-0">
+            <Image src="/fruits.svg" fill={true} alt="Panier équilibré repmlit de légumes et protéines" />
           </div>
           <div className="basis-1/2">
             <h2 className="font-epilogue font-extrabold text-lg lg:text-2xl lg:mt-5 lg:text-right">Variété de Saveurs Santé</h2>
@@ -67,14 +67,14 @@ export default function Home() {
       </section>
       <section className="h-full w-full lg:h-fit lg:flex lg:justify-center lg:align-center">
         <div className="px-6 text-center mt-10 py-5 lg:mt-0 lg:w-[80%] lg:flex lg:flex-row-reverse lg:items-center lg:justify-around">
-          <div className="w-60 h-64 relative mt-3 mb-6 mx-auto md:h-[23rem] lg:w-basis-1/3 lg:h-[30rem] lg:mt-0 lg:mx-0">
-            <Image src="/healthy.webp" fill={true} alt="Repas équilibré créé avec l'aide d'une intelligence artificielle" />
+          <div className="w-60 h-[11rem] relative mt-3 mb-6 mx-auto lg:basis-2/5 lg:h-[18rem] lg:mt-0 lg:mx-0">
+            <Image src="/gourmandise.webp" fill={true} alt="Repas équilibré créé avec l'aide d'une intelligence artificielle" />
           </div>
           <div className="basis-1/2">
             <h2 className="font-epilogue font-extrabold text-lg lg:text-2xl lg:mt-5 lg:text-left">Savourer la Gourmandise en Toute Santé</h2>
             <p className="font-inter mt-7 lg:text-xl lg:leading-7 lg:text-left">
             Rejoignez-nous dans cette aventure culinaire où la gourmandise et la santé se rencontrent harmonieusement.
-            <br /><br /><strong>ADAPTO&apos;DIET</strong>vous accompagne dans votre quête d&apos;un mode de vie équilibré, en mettant en avant le plaisir de la table tout en respectant vos objectifs diététiques.
+            <br /><br /><strong>ADAPTO&apos;DIET</strong> vous accompagne dans votre quête d&apos;un mode de vie équilibré, en mettant en avant le plaisir de la table tout en respectant vos objectifs diététiques.
             </p>
           </div>
         </div>
